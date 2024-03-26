57758a8fbe739850ab8bb307fa8d849dcbd3c2f9
riclovato <riic.lovato@gmail.com>
2024-03-26 19:37:58 -0300

    excluir e editar eventos

--- core/models.py
+++ core/models.py
@@ -6,9 +6,11 @@ from django.contrib.auth.models import User
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
+    local_evento = models.CharField(max_length=100, blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User,on_delete=models.CASCADE)
+
 
     class Meta:
         db_table = 'evento'
@@ -18,3 +20,7 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
+
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
